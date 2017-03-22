b57eb5c81fcf2621602462a58400648eca6e434d
orevron <orevron@gmail.com>
2017-03-22 13:26:56 +0200

    logger change

--- app/src/main/java/mobilab/mobilab/Logger.java
+++ app/src/main/java/mobilab/mobilab/Logger.java
@@ -7,12 +7,14 @@ import android.util.Log;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Logger extends Application {
     private final String TAG = "DEBUG";
     private static File logger;
     private static FileOutputStream fos;
-
 
     public Logger() throws IOException {
         logger = getOutputMediaFile();
@@ -21,7 +23,7 @@ public class Logger extends Application {
 
     public static void append(String message) {
         try {
-            fos.write((System.currentTimeMillis() + ":\t" + message + "\n").getBytes());
+            fos.write(("[" + (new Date(System.currentTimeMillis())) + "]:\t" + message + "\n").getBytes());
             Log.i("Logger:", message + "");
         } catch (IOException e) {
             e.printStackTrace();
@@ -42,7 +44,7 @@ public class Logger extends Application {
 
     public static void onDestroy() throws IOException {
         append("logger shut down.");
-         fos.close();
+        fos.close();
     }
 
 

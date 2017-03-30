377d0650d1fb02717be48d99d555c9c21a9b967d
orevron <orevron@gmail.com>
2017-03-31 02:24:51 +0300

    added constant variables

--- app/src/main/java/mobilab/mobilab/MainMenuActivity.java
+++ app/src/main/java/mobilab/mobilab/MainMenuActivity.java
@@ -18,30 +18,40 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class MainMenuActivity extends AppCompatActivity {
-    //const
-    public static final int CAMERA_10_INTERVAL = 2131493000;
-    public static final int CAMERA_30_INTERVAL = 2131493001;
-    public static final int CAMERA_60_INTERVAL = 2131493002;
-    public static final int CAMERA_640x480_RESOLUTION = 2131492995;
-    public static final int CAMERA_800x600_RESOLUTION = 2131492996;
-    public static final int CAMERA_1024x768_RESOLUTION = 2131492997;
-    public static final int SMS_10_INTERVAL = 2131493041;
-    public static final int SMS_30_INTERVAL = 2131493042;
-    public static final int SMS_60_INTERVAL = 2131493043;
-    public static final int SOUND_10_INTERVAL = 2131493050;
-    public static final int SOUND_30_INTERVAL = 2131493051;
-    public static final int SOUND_60_INTERVAL = 2131493052;
-    public static final int SOUND_30_DURATION = 2131493046;
-    public static final int SOUND_60_DURATION = 2131493047;
-    public static final int SOUND_120_DURATION = 2131493048;
-    public static final String GPS = "gps";
-    public static final String CAMERA = "camera";
-    public static final String SMS = "sms";
-    public static final String TEMPERATURE = "temperature";
-    public static final String BATTERY = "battery";
-    public static final String SOUND = "sound";
-    public static final String BAROMETER = "barometer";
-    public static final String EXTERNAL_SENSOR = "external sensor";
+    /**
+     * constants
+     */
+    private static final int CAMERA_10_INTERVAL = 2131493000;
+    private static final int CAMERA_30_INTERVAL = 2131493001;
+    private static final int CAMERA_60_INTERVAL = 2131493002;
+    private static final int CAMERA_640x480_RESOLUTION = 2131492995;
+    private static final int CAMERA_800x600_RESOLUTION = 2131492996;
+    private static final int CAMERA_1024x768_RESOLUTION = 2131492997;
+    private static final int SMS_10_INTERVAL = 2131493041;
+    private static final int SMS_30_INTERVAL = 2131493042;
+    private static final int SMS_60_INTERVAL = 2131493043;
+    private static final int SOUND_10_INTERVAL = 2131493050;
+    private static final int SOUND_30_INTERVAL = 2131493051;
+    private static final int SOUND_60_INTERVAL = 2131493052;
+    private static final int SOUND_30_DURATION = 2131493046;
+    private static final int SOUND_60_DURATION = 2131493047;
+    private static final int SOUND_120_DURATION = 2131493048;
+    private static final String GPS = "gps";
+    private static final String CAMERA = "camera";
+    private static final String SMS = "sms";
+    private static final String TEMPERATURE = "temperature";
+    private static final String BATTERY = "battery";
+    private static final String SOUND = "sound";
+    private static final String BAROMETER = "barometer";
+    private static final String EXTERNAL_SENSOR = "external sensor";
+    private static final String ID = "id";
+    private static final String INTERVAL = "interval";
+    private static final String TELEPHONE = "telephone";
+    private static final String RESOLUTION = "resolution";
+    private static final String DURATION = "duration";
+    private static final String DEFAULT_TEL_NUMBER = "0000000000";
+    private static final String OK = "OK";
+    Button startButton;
 
     /**
      * check box configuration object to hold configuration for each sensor
@@ -62,22 +72,18 @@ public class MainMenuActivity extends AppCompatActivity {
                 this.data = new HashMap<String, Object>();
                 alertDialog = true;
             }
-            if (name.equals(CAMERA))  //set default data for camera
-            {
-                this.data.put("interval", 30);
-                this.data.put("resolution", "640x480");
+            if (name.equals(CAMERA)) {  //set default data for camera
+                this.data.put(INTERVAL, 30);
+                this.data.put(RESOLUTION, "640x480");
             }
-            if (name.equals(SMS))    //set default data for SMS
-            {
-                this.data.put("telephone", "0000000000");
-                this.data.put("interval", "30");
+            if (name.equals(SMS)) {//set default data for SMS
+                this.data.put(TELEPHONE, DEFAULT_TEL_NUMBER);
+                this.data.put(INTERVAL, "30");
             }
-            if (name.equals(SOUND))    //set default data for sound
-            {
-                this.data.put("duration", "60");
-                this.data.put("interval", "30");
+            if (name.equals(SOUND)) {//set default data for sound
+                this.data.put(DURATION, "60");
+                this.data.put(INTERVAL, "30");
             }
-
         }
 
         public Boolean getState() {
@@ -94,7 +100,6 @@ public class MainMenuActivity extends AppCompatActivity {
 
     }
 
-    Button button;
 
     private static Logger logger;
     private config _gps, _camera, _sms, _temperature, _battery, _sound, _barometer, _externalSensors;
@@ -112,8 +117,8 @@ public class MainMenuActivity extends AppCompatActivity {
         initConfigurations();
         setAllListeners();
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        startButton = (Button) findViewById(R.id.button);
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 logger.append("this is start button.");
@@ -207,12 +212,12 @@ public class MainMenuActivity extends AppCompatActivity {
         final RadioGroup resolutionRadioGroup = (RadioGroup) dialog.findViewById(R.id.resolutionGroup);
 
         //Make radio interval button clicked:
-        int lastIntervalId = getResources().getIdentifier("" + R.string.CAMERA + conf.data.get("interval") + "sec", "id", getPackageName());
+        int lastIntervalId = getResources().getIdentifier(CAMERA + conf.data.get(INTERVAL) + "sec", ID, getPackageName());
         RadioButton LastRadioButton = (RadioButton) dialog.findViewById(lastIntervalId);
         LastRadioButton.setChecked(true);
 
         //Make radio resolution button clicked:
-        int lastResolutionId = getResources().getIdentifier("r" + conf.data.get(R.string.resolution2), "id", getPackageName());
+        int lastResolutionId = getResources().getIdentifier("r" + conf.data.get(RESOLUTION), ID, getPackageName());
         RadioButton LastResolutionButton = (RadioButton) dialog.findViewById(lastResolutionId);
         LastResolutionButton.setChecked(true);
 
@@ -223,33 +228,30 @@ public class MainMenuActivity extends AppCompatActivity {
                 int resolutionSelectedId = resolutionRadioGroup.getCheckedRadioButtonId();
 
                 if (intervalSelectedId == CAMERA_10_INTERVAL) {
-                    conf.changeData("interval", 10);
+                    conf.changeData(INTERVAL, 10);
                     Logger.append(CAMERA + R.string.interval_changed_10);
                 }
                 if (intervalSelectedId == CAMERA_30_INTERVAL) {
-                    conf.changeData("interval", 30);
+                    conf.changeData(INTERVAL, 30);
                     Logger.append(CAMERA + R.string.interval_changed_30);
                 }
                 if (intervalSelectedId == CAMERA_60_INTERVAL) {
-                    conf.changeData("interval", 60);
+                    conf.changeData(INTERVAL, 60);
                     Logger.append(CAMERA + R.string.interval_changed_60);
                 }
-
                 if (resolutionSelectedId == CAMERA_640x480_RESOLUTION) {
-                    conf.changeData("resolution", "640x480");
+                    conf.changeData(RESOLUTION, "640x480");
                     Logger.append(R.string.resolution_changed_640 + "");
                 }
                 if (resolutionSelectedId == CAMERA_800x600_RESOLUTION) {
-                    conf.changeData("resolution", "800x600");
+                    conf.changeData(RESOLUTION, "800x600");
                     Logger.append(R.string.resolution_changed_800 + "");
                 }
                 if (resolutionSelectedId == CAMERA_1024x768_RESOLUTION) {
-                    conf.changeData("resolution", "1024x768");
+                    conf.changeData(RESOLUTION, "1024x768");
                     Logger.append(R.string.resolution_changed_1024 + "");
                 }
-                Toast.makeText(getApplicationContext(), "Camera set to: " + conf.data.get("resolution") + " and " + conf.data.get("interval")
-                        + " sec", Toast.LENGTH_SHORT).show();
-
+                Toast.makeText(getApplicationContext(), "Camera set to: " + conf.data.get(RESOLUTION) + " and " + conf.data.get(INTERVAL) + " sec", Toast.LENGTH_SHORT).show();
                 dialog.dismiss();
             }
         });
@@ -272,13 +274,13 @@ public class MainMenuActivity extends AppCompatActivity {
         final EditText telephoneText = (EditText) dialog.findViewById(R.id.telNo);
 
         //Make radio interval button clicked:
-        int lastIntervalId = getResources().getIdentifier("sms" + conf.data.get("interval") + "sec", "id", getPackageName());
+        int lastIntervalId = getResources().getIdentifier(SMS + conf.data.get(INTERVAL) + "sec", ID, getPackageName());
         RadioButton LastRadioButton = (RadioButton) dialog.findViewById(lastIntervalId);
         LastRadioButton.setChecked(true);
 
         //Make telephoneText show the last data:
-        String lastTelephoneVal = (String) conf.data.get(R.string.telNo);
-        if (lastTelephoneVal != R.string.default_tel_number + "") {
+        String lastTelephoneVal = (String) conf.data.get(TELEPHONE);
+        if (!lastTelephoneVal.equals(DEFAULT_TEL_NUMBER)) {
             telephoneText.setText(lastTelephoneVal, TextView.BufferType.EDITABLE);
         }
 
@@ -289,15 +291,15 @@ public class MainMenuActivity extends AppCompatActivity {
                 int intervalSelectedId = SMSRadioGroup.getCheckedRadioButtonId();
 
                 if (intervalSelectedId == SMS_10_INTERVAL) {
-                    conf.changeData("interval", 10);
+                    conf.changeData(INTERVAL, 10);
                     Logger.append(SMS + R.string.interval_changed_10);
                 }
                 if (intervalSelectedId == SMS_30_INTERVAL) {
-                    conf.changeData("interval", 30);
+                    conf.changeData(INTERVAL, 30);
                     Logger.append(SMS + R.string.interval_changed_30);
                 }
                 if (intervalSelectedId == SMS_60_INTERVAL) {
-                    conf.changeData("interval", 60);
+                    conf.changeData(INTERVAL, 60);
                     Logger.append(SMS + R.string.interval_changed_60);
                 }
 
@@ -307,7 +309,7 @@ public class MainMenuActivity extends AppCompatActivity {
                     AlertDialog alertDialog = new AlertDialog.Builder(MainMenuActivity.this).create();
                     alertDialog.setTitle(R.string.enter_phone_number);
                     alertDialog.setMessage(R.string.enter_phone_number_description + "");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, OK,
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
                                     dialog.dismiss();
@@ -318,7 +320,7 @@ public class MainMenuActivity extends AppCompatActivity {
                     AlertDialog alertDialog = new AlertDialog.Builder(MainMenuActivity.this).create();
                     alertDialog.setTitle(R.string.enter_phone_number);
                     alertDialog.setMessage(R.string.enter_phone_number_error + "");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, OK,
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
                                     dialog.dismiss();
@@ -326,8 +328,8 @@ public class MainMenuActivity extends AppCompatActivity {
                             });
                     alertDialog.show();
                 } else {
-                    conf.changeData("telephone", telephoneNumber);
-                    Toast.makeText(getApplicationContext(), "SMS will set to: " + conf.data.get("telephone") + " every " + conf.data.get("interval")
+                    conf.changeData(TELEPHONE, telephoneNumber);
+                    Toast.makeText(getApplicationContext(), "SMS will set to: " + conf.data.get(TELEPHONE) + " every " + conf.data.get(INTERVAL)
                             + " sec", Toast.LENGTH_SHORT).show();
 
                     dialog.dismiss();
@@ -352,12 +354,12 @@ public class MainMenuActivity extends AppCompatActivity {
         final RadioGroup durationRadioGroup = (RadioGroup) dialog.findViewById(R.id.durationGroup);
 
         //Make radio interval button clicked:
-        int lastIntervalId = getResources().getIdentifier("iDuration" + conf.data.get("interval") + "sec", "id", getPackageName());
+        int lastIntervalId = getResources().getIdentifier("iDuration" + conf.data.get(INTERVAL) + "sec", ID, getPackageName());
         RadioButton LastRadioButton = (RadioButton) dialog.findViewById(lastIntervalId);
         LastRadioButton.setChecked(true);
 
         //Make radio duration button clicked:
-        int lastDurationId = getResources().getIdentifier("sDuration" + conf.data.get("duration") + "sec", "id", getPackageName());
+        int lastDurationId = getResources().getIdentifier("sDuration" + conf.data.get(DURATION) + "sec", ID, getPackageName());
         RadioButton LastDurationButton = (RadioButton) dialog.findViewById(lastDurationId);
         LastDurationButton.setChecked(true);
 
@@ -368,31 +370,31 @@ public class MainMenuActivity extends AppCompatActivity {
                 int DurationSelectedId = durationRadioGroup.getCheckedRadioButtonId();
 
                 if (intervalSelectedId == SOUND_10_INTERVAL) {
-                    conf.changeData("interval", 30);
+                    conf.changeData(INTERVAL, 30);
                     Logger.append(SOUND + R.string.interval_changed_10);
                 }
                 if (intervalSelectedId == SOUND_30_INTERVAL) {
-                    conf.changeData("interval", 60);
+                    conf.changeData(INTERVAL, 60);
                     Logger.append(SOUND + R.string.interval_changed_30);
                 }
                 if (intervalSelectedId == SOUND_60_INTERVAL) {
-                    conf.changeData("interval", 120);
+                    conf.changeData(INTERVAL, 120);
                     Logger.append(SOUND + R.string.interval_changed_60);
                 }
 
                 if (DurationSelectedId == SOUND_30_DURATION) {
-                    conf.changeData("duration", "30");
+                    conf.changeData(DURATION, "30");
                     Logger.append(SOUND + R.string.duration_changed_30);
                 }
                 if (DurationSelectedId == SOUND_60_DURATION) {
-                    conf.changeData("duration", "60");
+                    conf.changeData(DURATION, "60");
                     Logger.append(SOUND + R.string.duration_changed_60);
                 }
                 if (DurationSelectedId == SOUND_120_DURATION) {
-                    conf.changeData("duration", "120");
+                    conf.changeData(DURATION, "120");
                     Logger.append(SOUND + R.string.duration_changed_120);
                 }
-                Toast.makeText(getApplicationContext(), "Sound set to duration: " + conf.data.get("duration") + "sec, every " + conf.data.get("interval")
+                Toast.makeText(getApplicationContext(), "Sound set to duration: " + conf.data.get(DURATION) + "sec, every " + conf.data.get(INTERVAL)
                         + " sec", Toast.LENGTH_SHORT).show();
                 dialog.dismiss();
             }
@@ -410,6 +412,3 @@ public class MainMenuActivity extends AppCompatActivity {
         }
     }
 }
-
-
-

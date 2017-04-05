afde3d486a1ee5ab5a3ebe1c9671385dbb95a071
orevron <orevron@gmail.com>
2017-04-05 17:18:06 +0300

    static strings in logger class

--- app/src/main/java/mobilab/mobilab/Logger.java
+++ app/src/main/java/mobilab/mobilab/Logger.java
@@ -12,7 +12,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Logger extends Application {
-    private final String TAG = "DEBUG";
+    private static final String NAME = "MobiLAB";
+    private static final String FILE_NAME = "mainlog.txt";
+    private static final String SHUTDOWN = "logger shut down.";
+    private static final String ERROR = "failed to create directory";
+    private static final String TAG = "DEBUG";
+    private static final String _logger = "Logger: ";
     private static File logger;
     private static FileOutputStream fos;
 
@@ -24,28 +29,26 @@ public class Logger extends Application {
     public static void append(String message) {
         try {
             fos.write(("[" + (new Date(System.currentTimeMillis())) + "]:\t" + message + "\n").getBytes());
-            Log.i("Logger:", message + "");
+            Log.i(_logger, message + "");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     private File getOutputMediaFile() {
-        File mediaStorageDir = new File(Environment.getExternalStorageDirectory(), "MobiLAB");
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory(), NAME);
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
-                Log.e(TAG, "failed to create directory");
+                Log.e(TAG, ERROR);
                 return null;
             }
         }
         // Create a media file name
-        return new File(mediaStorageDir.getPath() + File.separator + "mainlog.txt");
+        return new File(mediaStorageDir.getPath() + File.separator + FILE_NAME);
     }
 
     public static void onDestroy() throws IOException {
-        append("logger shut down.");
+        append(SHUTDOWN);
         fos.close();
     }
-
-
 }
